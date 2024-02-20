738f50bddc17e86abab861731ec080212e7faf30
Andrew Meyer <andrew.meyer003@gmail.com>
2024-02-20 13:25:05 +0000

    setup 2

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,8 +1,9 @@
 function Homepage() {
   return (
-    <div>
-      <h1 className="text-8xl">Welcome to the homepage!</h1>
-    </div>
+    <>
+        <h1>Welcome to the homepage</h1>
+        {/* hyperui.dev */}
+    </>
   );
 }  
 
