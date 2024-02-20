76ff71afff924ba18b32e4236ddeb1bb5a5dc1a9
Andrew Meyer <andrew.meyer003@gmail.com>
2024-02-20 14:44:36 +0000

    Added Sidebar.js

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,7 +1,10 @@
+import Sidebar from '../Sidebar/Sidebar';
+
 function Homepage() {
   return (
     <>
         <h1>Welcome to the homepage</h1>
+        <Sidebar />
         {/* hyperui.dev */}
     </>
   );
