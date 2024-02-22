b7efff6a94ce77855733457274936e525db8aabc
Andrew Meyer <andrew.meyer003@gmail.com>
2024-02-22 15:14:06 +0000

    sidebar styling

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 function Sidebar(){
     return(
-    <ul className ="group space-y-1 h-screen bg-white w-20 hover:w-1/6 transition-all duration-1000 hover:bg-gray-100">
-        <li className="group group-hover:block block w-1/6 hover:w-1/6 rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-            <div className="group-hover transition-all duration-1000">
-                <div className="transition-all group-hover:w-7 w-5 h-0.5 bg-black m-2"></div>
-                <div className="transition-all group-hover:w-3 w-5 h-0.5 bg-black m-2"></div>
-                <div className="transition-all group-hover:w-7 w-5 h-0.5 bg-black m-2"></div>
+    <ul className ="group space-y-1 h-screen w-20 hover:w-1/6 transition-all duration-1000">
+        <li className="group group-hover:block block w-1/6 hover:w-1/6 rounded-lg px-4 py-2 text-sm font-medium">
+            <div className="group-hover">
+                <div className="transition-all duration-1000 group-hover:w-7 w-5 h-0.5 bg-black m-2"></div>
+                <div className="transition-all duration-1000 group-hover:w-3 w-5 h-0.5 bg-black m-2"></div>
+                <div className="transition-all duration-1000 group-hover:w-7 w-5 h-0.5 bg-black m-2"></div>
             </div>
         </li>
-        <li>
-            <Link to="/" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+        <li className="">
+            <Link to="/" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-sm font-medium text-gray-500 bg-grey-100 hover:bg-gray-100 hover:text-gray-700">
             Home
             </Link>
         </li>
