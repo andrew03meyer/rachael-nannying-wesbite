7f5e3f858717b97099b8da2e4810b13c3755dd4c
Andrew Meyer <am2660@kent.ac.uk>
2024-02-23 23:06:12 +0000

    aesthetics

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -8,7 +8,7 @@ import FacebookLogo from "../assets/images/facebook_logo.svg";
 const images = [NannyPic, NannyPic2, GithubLogo, InstagramLogo, FacebookLogo];
 const captions = ["Quality care for your loved ones", "We are here to help", "howdy", "message", "hi how are you?"];
 
-function Homepage() {
+function Carousel() {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
@@ -21,10 +21,10 @@ function Homepage() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center relative w-screen h-auto max-h-screen  overflow-y-hidden">
-      <img id="image" src={images[index]} alt="Nanny" className="w-full h-auto fade-in-out" />
+    <div className="flex flex-col items-center justify-center relative w-screen h-auto max-h-screen overflow-y-hidden">
+      <img id="image" src={images[index]} alt="Nanny" className="w-full h-auto" />
       <div className="absolute m-10">
-        <h2 className="text-2xl">{captions[index]}</h2>
+        <h2 className="text-7xl text-orange-50 font-bold">{captions[index]}</h2>
       </div>
       <div className="absolute w-14 flex flex-col justify-center items-center right-0 float-end m-10 items bg-orange-50 rounded-2xl">
         <img src={GithubLogo} alt="Github" className="w-10 h-10 m-2 hover:w-12 hover:h-12 transition-all duration-500" />
@@ -35,4 +35,4 @@ function Homepage() {
   );
 }
 
-export default Homepage;
+export default Carousel;
