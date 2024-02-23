d6706a7692c6e386c933faf2d254fb467bf70b7d
Andrew Meyer <am2660@kent.ac.uk>
2024-02-23 22:19:53 +0000

    sidebar styling

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Sidebar(){
     return(
-    <ul className ="group space-y-1 h-screen w-20 hover:w-1/6 transition-all duration-1000">
+    <ul className ="group space-y-1 h-screen w-20 hover:w-1/6 transition-all duration-800">
         <li className="group group-hover:block block w-1/6 hover:w-1/6 rounded-lg px-4 py-2 text-sm font-medium">
             <div className="group-hover">
                 <div className="transition-all duration-1000 group-hover:w-7 w-5 h-0.5 bg-black m-2"></div>
@@ -11,19 +11,19 @@ function Sidebar(){
             </div>
         </li>
         <li className="">
-            <Link to="/" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-sm font-medium text-gray-500 bg-grey-100 hover:bg-gray-100 hover:text-gray-700">
+            <Link to="/" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-xl hover:text-2xl transition-all duration-800 font-medium text-gray-500 bg-grey-100 hover:bg-gray-100 hover:text-gray-700">
             Home
             </Link>
         </li>
 
         <li>
-            <Link to="/about" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+            <Link to="/about" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-xl hover:text-2xl transition-all duration-800 font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
             About
             </Link>
         </li>
 
         <li>
-            <Link to="/rates" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+            <Link to="/rates" className="group-hover:block hidden truncate rounded-lg px-4 py-2 text-xl hover:text-2xl transition-all duration-800 font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
             Rates
             </Link>
         </li>
