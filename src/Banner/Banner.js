ce135d63712632001566454dfa56623e7ec70b47
Andrew Meyer <andrew.meyer003@gmail.com>
2024-03-03 15:36:00 +0000

    failed fix: hover icon sidebar disappeared

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,18 +2,18 @@ function Banner(){
     return (
         <>
         {/* <div className="h-20 w-full bg-gradient-to-b from-orange-200 via-orange-300 to-orange-300"></div> */}
-        <div className="flex justify-center h-20 w-full bg-gradient-to-b from-white to-orange-100 font-roboto text-md italic items-center p-2 text-white">
+        <div className="flex justify-center h-20 w-full bg-gradient-to-b from-white via-orange-100 to-orange-100 font-roboto text-md italic items-center p-2 text-white">
         {/* bg-gradient-to-bl from-orange-300 via-orange-200 to-orange-300 */}
-            <table className="w-5/6 h-5/6 text-center">
+            <table className="w-5/6 h-5/6 text-center text-gray-400 text-sm">
                 <tbody>
                     <tr>
-                        <td className="w-32"><a href="/" className="hover:underline text-gray-400 hover:text-gray-500 items-center">Home</a></td>
-                        <td className="text-orange-300">|</td>
-                        <td className="w-32"><a href="/about" className="hover:underline text-gray-400 hover:text-gray-500 items-center">About</a></td>
-                        <td className="text-orange-300">|</td>
-                        <td className="w-32"><a href="/rates" className="hover:underline text-gray-400 hover:text-gray-500 items-center">Rates</a></td>
-                        <td className="text-orange-300">|</td>
-                        <td className="w-32"><a href="/contact" className="hover:underline text-gray-400 hover:text-gray-500 items-center">Contact Us</a></td>
+                        <td className="w-32">@ Copyright Rachael Nannying 2024/2025</td>
+                    </tr>
+                    <tr>
+                        <td className="w-32">All material on website used with consent</td>
+                    </tr>
+                    <tr>
+                        <td className="w-32">Website built and maintained by Andrew Meyer</td>
                     </tr>
                 </tbody>
             </table>
